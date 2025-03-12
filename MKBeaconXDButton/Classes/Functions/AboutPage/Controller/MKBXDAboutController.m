acd9cc890f0c2476d3cb57984803fd9987375a82
lovexiaoxia <aadyx2007@163.com>
2025-03-12 12:04:25 +0800

    Fix  Version

--- MKBeaconXDButton/Classes/Functions/AboutPage/Controller/MKBXDAboutController.m
+++ MKBeaconXDButton/Classes/Functions/AboutPage/Controller/MKBXDAboutController.m
@@ -123,7 +123,7 @@
         _versionLabel.textColor = RGBCOLOR(189, 189, 189);
         _versionLabel.textAlignment = NSTextAlignmentCenter;
         _versionLabel.font = MKFont(16.f);
-        _versionLabel.text = [@"Version: V" stringByAppendingString:kAppVersion];
+        _versionLabel.text = @"Version: V2.0.2";
     }
     return _versionLabel;
 }
